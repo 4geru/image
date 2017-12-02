08497fdad1d71ce6f04e71f60dde25476ae51005
Rp7rf <westhouse51@gmail.com>
2017-12-02 19:25:24 +0900

    fix some

--- app.rb
+++ app.rb
@@ -9,11 +9,13 @@ get '/' do
 	erb :index
 end
 post '/image_upload' do
+  name = params[:images]['filename']
+  puts name
 	image_upload_local(params[:images])
 	redirect'/image'
 end
 get '/image' do
 	erb :image
 end
-# path = ARGV[0]
-# system('python detect.py ' + path)
+path = ARGV[0]
+system('python detect.py ' + path)
